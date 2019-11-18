075bec4f1de9291b76371c1d9d14fbfdc0287600
Philippe <phil.herail@gmail.com>
2019-11-18 12:09:00 +0100

    format

--- ArduinoUnoNano-downlink-custom/ArduinoUnoNano-downlink-custom.ino
+++ ArduinoUnoNano-downlink-custom/ArduinoUnoNano-downlink-custom.ino
@@ -143,12 +143,11 @@ void loop()
         triggered = false;
         Serial.println("Interrupt worked !");
 
-    //Get gas sensor data
-    // TODO Modify the code to get data when the interrupt is fired
-    sensor_value = String(getGasSensorVoltage());
-    Serial.println("Sensor value: " + sensor_value);
-    sendData(sensor_value);
-
+        //Get gas sensor data
+        // TODO Modify the code to get data when the interrupt is fired
+        sensor_value = String(getGasSensorVoltage());
+        Serial.println("Sensor value: " + sensor_value);
+        sendData(sensor_value);
     }
     delay(1000);
 
@@ -187,13 +186,13 @@ float getGasSensorVoltage()
 void setupAcomp()
 {
     ACSR =
-        (0 << ACD) |                // Analog Comparator: Enabled
-        (0 << ACBG) |               // Analog Comparator Bandgap Select: AIN0 is applied to the positive input
-        (0 << ACO) |                // Analog Comparator Output: Off
-        (1 << ACI) |                // Analog Comparator Interrupt Flag: Clear Pending Interrupt
-        (1 << ACIE) |               // Analog Comparator Interrupt: Enabled
-        (0 << ACIC) |               // Analog Comparator Input Capture: Disabled
-        (1 << ACIS1) | (1 < ACIS0); // Analog Comparator Interrupt Mode: Comparator Interrupt on Rising Output Edge
+        (0 << ACD)                    // Analog Comparator: Enabled
+        | (0 << ACBG)                 // Analog Comparator Bandgap Select: AIN0 is applied to the positive input
+        | (0 << ACO)                  // Analog Comparator Output: Off
+        | (1 << ACI)                  // Analog Comparator Interrupt Flag: Clear Pending Interrupt
+        | (1 << ACIE)                 // Analog Comparator Interrupt: Enabled
+        | (0 << ACIC)                 // Analog Comparator Input Capture: Disabled
+        | (1 << ACIS1) | (1 < ACIS0); // Analog Comparator Interrupt Mode: Comparator Interrupt on Rising Output Edge
 }
 
 //Sends data with LoRa
