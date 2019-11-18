9fe2c45548b64bab105a35031b6107616ede22da
lucie <lucie.bechtet@laposte.net>
2019-11-18 08:31:06 +0100

    with the test for gas sensor

--- ArduinoUnoNano-downlink-custom/ArduinoUnoNano-downlink-custom.ino
+++ ArduinoUnoNano-downlink-custom/ArduinoUnoNano-downlink-custom.ino
@@ -161,6 +161,20 @@ void loop()
     delay(5000);
 }
 
+void loop() {
+    float sensor_volt;
+    float sensorValue;
+
+    sensorValue = analogRead(A0);
+    sensor_volt = sensorValue/1024*5.0;
+
+    Serial.print("sensor_volt = ");
+    Serial.print(sensor_volt);
+    Serial.println("V");
+    delay(1000);
+}
+
+
 void led_on()
 {
   digitalWrite(13, 1);
